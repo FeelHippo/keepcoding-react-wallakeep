541191c30f2672e5ab1112fe5c6fc9f8d6d8257f
David Jiménez <davidjj76@gmail.com>
2019-12-15 00:33:32 +0100

    Refactor de formularios

--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -1,125 +1,29 @@
-import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
+import React from 'react';
 
 import Layout from '../Layout/Layout';
 import FormControl from '@material-ui/core/FormControl';
 import Button from '@material-ui/core/Button';
-import Input from '@material-ui/core/Input';
+import MaterialUiInput from '@material-ui/core/Input';
 import InputLabel from '@material-ui/core/InputLabel';
 import SaveIcon from '@material-ui/icons/Save';
 import DeleteIcon from '@material-ui/icons/Delete';
+
+import Form, { Input } from '../Form';
 
 import Session from '../../models/Session';
 import imagePhoto from '../../assets/images/user.png';
 
 import './Profile.css';
 
-export default class Profile extends Component {
-  constructor(props) {
-    super(props);
-    this.state = props.session;
-  }
-
-  render() {
-    const { name, surname, maxAdverts } = this.state;
-    return (
-      <Layout sectionTitle="Perfil de usuario">
-        <form
-          onSubmit={this.handleSubmit}
-          noValidate
-          autoComplete="off"
-          className="Profile__Form"
-        >
-          <div className="Profile_Picture">
-            <img src={imagePhoto} alt="user_avatar" />
-          </div>
-          <FormControl fullWidth className="Profile__FormControl">
-            <InputLabel shrink htmlFor="type">
-              Nombre
-            </InputLabel>
-            <Input
-              name="name"
-              value={name}
-              onChange={this.handleChange}
-              type="text"
-              required
-            />
-          </FormControl>
-          <FormControl fullWidth className="Profile__FormControl">
-            <InputLabel shrink htmlFor="type">
-              Apellido
-            </InputLabel>
-            <Input
-              name="surname"
-              value={surname}
-              onChange={this.handleChange}
-              type="text"
-              required
-            />
-          </FormControl>
-          <FormControl fullWidth className="Profile__FormControl">
-            <InputLabel htmlFor="maxAdverts">
-              Anuncios por página (Home)
-            </InputLabel>
-            <Input
-              name="maxAdverts"
-              type="number"
-              value={maxAdverts}
-              onChange={this.handleChange}
-              min={1}
-              max={20}
-            />
-          </FormControl>
-          <div className="Profile__Footer">
-            <Button
-              type="submit"
-              variant="contained"
-              color="primary"
-              startIcon={<SaveIcon />}
-              className="ButtonWallakeep ButtonWallakeep__Green"
-            >
-              Guardar
-            </Button>
-            <Button
-              type="button"
-              variant="contained"
-              color="secondary"
-              onClick={this.props.userLogout}
-              startIcon={<DeleteIcon />}
-            >
-              Borrar
-            </Button>
-          </div>
-        </form>
-      </Layout>
-    );
-  }
-
-  handleChange = ({ target }) => {
-    const { name, type, checked, value } = target;
-    this.setState({
-      [name]: type === 'checkbox' ? checked : value,
-    });
-  };
-
-  handleSubmit = ev => {
-    const { session, saveSession, enqueueSnackbar } = this.props;
-    const { name, surname, maxAdverts } = this.state;
+export default function Profile({
+  session,
+  saveSession,
+  enqueueSnackbar,
+  userLogout,
+}) {
+  const handleSubmit = value => {
+    const { name, surname, maxAdverts } = value;
     const parsedMaxAdverts = parseInt(maxAdverts);
-    ev.preventDefault();
-
-    if (
-      !name ||
-      !surname ||
-      !Number.isInteger(parsedMaxAdverts) ||
-      parsedMaxAdverts <= 0
-    ) {
-      enqueueSnackbar('Rellene todos los campos del formulario', {
-        variant: 'error',
-      });
-      return;
-    }
-
     // Genero sesión y la guardo en LS
     saveSession(
       new Session(name, surname, session.apiUrl, parsedMaxAdverts),
@@ -129,4 +33,75 @@ export default class Profile extends Component {
       variant: 'success',
     });
   };
+
+  return (
+    <Layout sectionTitle="Perfil de usuario">
+      <Form
+        initialValue={session}
+        validate={({ name, surname, maxAdverts }) => {
+          const parsedMaxAdverts = parseInt(maxAdverts);
+          if (
+            !name ||
+            !surname ||
+            !Number.isInteger(parsedMaxAdverts) ||
+            parsedMaxAdverts <= 0
+          ) {
+            return 'Rellene todos los campos del formulario';
+          }
+        }}
+        onSubmit={handleSubmit}
+        onError={error => enqueueSnackbar(error, { variant: 'error' })}
+        autoComplete="off"
+        className="Profile__Form"
+      >
+        <div className="Profile_Picture">
+          <img src={imagePhoto} alt="user_avatar" />
+        </div>
+        <FormControl fullWidth className="Profile__FormControl">
+          <InputLabel shrink htmlFor="type">
+            Nombre
+          </InputLabel>
+          <Input name="name" type="text" component={MaterialUiInput} />
+        </FormControl>
+        <FormControl fullWidth className="Profile__FormControl">
+          <InputLabel shrink htmlFor="type">
+            Apellido
+          </InputLabel>
+          <Input name="surname" type="text" component={MaterialUiInput} />
+        </FormControl>
+        <FormControl fullWidth className="Profile__FormControl">
+          <InputLabel htmlFor="maxAdverts">
+            Anuncios por página (Home)
+          </InputLabel>
+          <Input
+            name="maxAdverts"
+            type="number"
+            min={1}
+            max={20}
+            component={MaterialUiInput}
+          />
+        </FormControl>
+        <div className="Profile__Footer">
+          <Button
+            type="submit"
+            variant="contained"
+            color="primary"
+            startIcon={<SaveIcon />}
+            className="ButtonWallakeep ButtonWallakeep__Green"
+          >
+            Guardar
+          </Button>
+          <Button
+            type="button"
+            variant="contained"
+            color="secondary"
+            onClick={userLogout}
+            startIcon={<DeleteIcon />}
+          >
+            Borrar
+          </Button>
+        </div>
+      </Form>
+    </Layout>
+  );
 }
--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -1,116 +1,90 @@
-import React, { Component } from 'react';
+import React from 'react';
 import FormControlLabel from '@material-ui/core/FormControlLabel';
 import AccountCircleIcon from '@material-ui/icons/AccountCircle';
 import InputAdornment from '@material-ui/core/InputAdornment';
 import FormControl from '@material-ui/core/FormControl';
 import Checkbox from '@material-ui/core/Checkbox';
 import Button from '@material-ui/core/Button';
-import Input from '@material-ui/core/Input';
+import MaterialUiInput from '@material-ui/core/Input';
+
+import Form, { Input } from '../Form';
 
 import Session from '../../models/Session';
 import imageLogo from '../../assets/images/logo2.png';
 import './Register.css';
 
-export default class Register extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      name: '',
-      surname: '',
-      remember: false,
-    };
-  }
-
-  render() {
-    const { name, surname, remember } = this.state;
-    const { endAdornment } = this.props;
-    return (
-      <div className="Register">
-        <div className="Register__Wrapper">
-          <form className="Register__Form" onSubmit={this.handleSubmit}>
-            <img
-              src={imageLogo}
-              className="Register__Logo"
-              alt="nodepop-logo"
-            />
-            <FormControl>
-              <Input
-                name="name"
-                value={name}
-                onChange={this.handleChange}
-                type="text"
-                placeholder="type your name"
-                autoComplete="username"
-                startAdornment={
-                  <InputAdornment position="start" className="InputIcon__Icon">
-                    <AccountCircleIcon />
-                  </InputAdornment>
-                }
-                endAdornment={endAdornment}
-                required
-              />
-            </FormControl>
-            <FormControl>
-              <Input
-                name="surname"
-                value={surname}
-                onChange={this.handleChange}
-                type="text"
-                placeholder="type your surname"
-                startAdornment={
-                  <InputAdornment position="start" className="InputIcon__Icon">
-                    <AccountCircleIcon />
-                  </InputAdornment>
-                }
-                endAdornment={endAdornment}
-                required
-              />
-            </FormControl>
-            <FormControlLabel
-              name="remember"
-              label="remember me"
-              control={
-                <Checkbox
-                  color="primary"
-                  checked={remember}
-                  onChange={this.handleChange}
-                />
-              }
-            />
-            <Button
-              className="button"
-              type="submit"
-              variant="contained"
-              color="primary"
-            >
-              Login
-            </Button>
-          </form>
-        </div>
-      </div>
-    );
-  }
-
-  handleSubmit = async event => {
-    const { userLogin, enqueueSnackbar } = this.props;
-    const { name, surname, remember } = this.state;
-    event.preventDefault();
-
-    // Validación básica del formulario
-    if (!name || !surname) {
-      enqueueSnackbar('Rellene todos los campos del formulario', {
-        variant: 'error',
-      });
-      return;
-    }
+export default function Register({ userLogin, enqueueSnackbar, endAdornment }) {
+  const handleSubmit = value => {
+    const { name, surname, remember } = value;
     // Genero sesión y la guardo en LS si ha seleccionado "remember"
     userLogin(new Session(name, surname), remember);
   };
 
-  handleChange = ({ target }) => {
-    const { name, type, checked, value } = target;
-    this.setState({
-      [name]: type === 'checkbox' ? checked : value,
-    });
-  };
+  return (
+    <div className="Register">
+      <div className="Register__Wrapper">
+        <Form
+          className="Register__Form"
+          initialValue={{ name: '', surname: '', remember: false }}
+          validate={({ name, surname }) => {
+            if (!name || !surname) {
+              return 'Rellene todos los campos del formulario';
+            }
+          }}
+          onSubmit={handleSubmit}
+          onError={error =>
+            enqueueSnackbar(error, {
+              variant: 'error',
+            })
+          }
+        >
+          <img src={imageLogo} className="Register__Logo" alt="nodepop-logo" />
+          <FormControl>
+            <Input
+              name="name"
+              type="text"
+              placeholder="type your name"
+              autoComplete="username"
+              startAdornment={
+                <InputAdornment position="start" className="InputIcon__Icon">
+                  <AccountCircleIcon />
+                </InputAdornment>
+              }
+              endAdornment={endAdornment}
+              component={MaterialUiInput}
+            />
+          </FormControl>
+          <FormControl>
+            <Input
+              name="surname"
+              type="text"
+              placeholder="type your surname"
+              startAdornment={
+                <InputAdornment position="start" className="InputIcon__Icon">
+                  <AccountCircleIcon />
+                </InputAdornment>
+              }
+              endAdornment={endAdornment}
+              component={MaterialUiInput}
+            />
+          </FormControl>
+          <Input
+            name="remember"
+            type="checkbox"
+            label="remember me"
+            control={<Checkbox color="primary" />}
+            component={FormControlLabel}
+          />
+          <Button
+            className="button"
+            type="submit"
+            variant="contained"
+            color="primary"
+          >
+            Login
+          </Button>
+        </Form>
+      </div>
+    </div>
+  );
 }
